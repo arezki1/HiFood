92221b0831031f307774f9f02ac1401814724386
arezki <a.ibsaine@hotmail.fr>
2017-12-05 20:49:32 +0000

    Adding the files

---
+++ registration/common.php
@@ -0,0 +1,68 @@
+<!--http://forums.devshed.com/php-faqs-stickies-167/program-basic-secure-login-system-using-php-mysql-891201.html -->
+<!--http://www.formget.com/login-form-in-php-->
+
+<?php 
+
+    // These variables define the connection information fo MySQL database 
+    $username = "arezki"; 
+    $password = ""; 
+    $host = "127.0.0.1"; 
+    $dbname ="hotel"; 
+    
+
+
+    
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+     
+ 
+    try 
+    { 
+        // This statement opens a connection to the database using the PDO library 
+        // PDO is designed to provide a flexible interface between PHP and many 
+        // different types of database servers. 
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
+    } 
+    catch(PDOException $ex) 
+    { 
+        // If an error occurs while opening a connection the database, it will 
+        // be trapped here
+        die("Failed to connect to the database: " . $ex->getMessage());
+    } 
+     
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+     
+    
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+     
+   
+    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) 
+    { 
+        function undo_magic_quotes_gpc(&$array) 
+        { 
+            foreach($array as &$value) 
+            { 
+                if(is_array($value)) 
+                { 
+                    undo_magic_quotes_gpc($value); 
+                } 
+                else 
+                { 
+                    $value = stripslashes($value); 
+                } 
+            } 
+        } 
+     
+        undo_magic_quotes_gpc($_POST); 
+        undo_magic_quotes_gpc($_GET); 
+        undo_magic_quotes_gpc($_COOKIE); 
+    } 
+     
+    // This tells the web browser that the content is encoded using UTF-8 
+
+    header('Content-Type: text/html; charset=utf-8'); 
+     
+
+    session_start(); 
+
+   
---
+++ registration/login.php
@@ -0,0 +1,135 @@
+<!--http://forums.devshed.com/php-faqs-stickies-167/program-basic-secure-login-system-using-php-mysql-891201.html -->
+<!--http://www.formget.com/login-form-in-php-->
+<?php 
+
+    // First we execute our common code to connection to the database and start the session 
+    require("common.php"); 
+     
+    // This variable will be used to re-display the user's username to them in the 
+    // login form if they fail to enter the correct password.  
+    $submitted_username = ''; 
+     
+    // check to determine whether the login form has been submitted 
+    // If it has, then the login code is run, otherwise the form is displayed 
+    if(!empty($_POST)) 
+    { 
+        // This query retreives the user's information from the database using 
+        // their username. 
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM user
+            WHERE 
+                username = :username 
+        "; 
+         
+        // The parameter values 
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try 
+        { 
+            // Execute the query against the database 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex) 
+        { 
+             
+            die("Failed to run query: " . $ex->getMessage()); 
+        } 
+         
+        // This variable tells us whether the user has successfully logged in or not. 
+        // We initialize it to false, assuming they have not.
+        $login_ok = false; 
+         
+        // Retrieve the user data from the database.  If $row is false, then the username 
+        // they entered is not registered. 
+        $row = $stmt->fetch(); 
+        if($row) 
+        { 
+            // Check if passwords match with hash and salt
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++) 
+            { 
+                $check_password = hash('sha256', $check_password . $row['salt']); 
+            } 
+             
+            if($check_password === $row['password']) 
+            { 
+                // If they do, then we flip this to true 
+                $login_ok = true; 
+            } 
+        } 
+         
+        // If the user logged in successfully, then we send them to the private members-only page 
+        // Otherwise, we display a login failed message and show the login form again 
+        if($login_ok) 
+        { 
+            // Here we are preparing to store the $row array into the $_SESSION by 
+            // removing the salt and password values from it.  Although $_SESSION is 
+            // stored on the server-side, there is no reason to store sensitive values 
+            // in it unless you have to.  Thus, it is best practice to remove these 
+            // sensitive values first. 
+            unset($row['salt']); 
+            unset($row['password']); 
+             
+            // This stores the user's data into the session at the index 'user'. 
+            // We will check this index on the private members-only page to determine whether 
+            // or not the user is logged in.  We can also use it to retrieve 
+            // the user's details. 
+            $_SESSION['user'] = $row; 
+             
+            // Redirect the user to the private members-only page. 
+            header("Location: hello-world.php"); 
+            die("Redirecting to: ../hello-world.php"); 
+        } 
+        else 
+        { 
+            // Tell the user they failed 
+            print("Login Failed."); 
+             
+            // Show them their username again so all they have to do is enter a new 
+            // password.
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+     
+?> 
+
+    <div class  = "container bgImg coolFont box">
+        <div class="row">
+            <h1 class = "text-center" id="mainh1">Social Hotel</h1>
+        <div class = "col-md-4">
+        </div>
+        <div class = "col-md-4" class="backgroundImage">
+            <h1>Login</h1> 
+                <form action="login.php" method="post"> 
+                    Username:<br /> 
+                    <input type="text" name="username"/> 
+                    <br /><br /> 
+                    Password:<br /> 
+                    <input type="password" name="password" value="" /> 
+                    <br /><br /> 
+                    <input type="submit" value="Login" /> 
+                </form> <br/>
+                <button class="regLogButton"><a href="register.php" id="reg">Register</a></button>
+                <br/><br/><br/><br/><br/><br/>
+        </div>
+        
+        <div class = "col-md-4">
+            
+        </div>
+        </div>
+        
+    </div>
+        
+    </body>
+    <br/>
+         
+       <footer class = "footer text-center ">&copy; Group C NCI 2016</footer
